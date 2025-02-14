0e68ed2fb4698c3c84b73bfa559f2adb893be8d5
Maxwell Morrissey <mmorrissey92@proton.me>
2025-02-14 12:31:30 -0500

    Block production env server redirect logs

--- mimir.mjs
+++ mimir.mjs
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+process.env.NODE_ENV = "production";
+
 (async () => {
     await import("./dist/root.js");
 })();
--- src/common/redirect.ts
+++ src/common/redirect.ts
@@ -1,13 +1,16 @@
 import fetch from "node-fetch";
 import express from "express";
 import { serverLogger } from "../server/middleware.js";
+import { NodeEnv } from "../env/Env.js";
 
 export const redirect = async (
     req: express.Request,
     res: express.Response,
     url: string,
 ) => {
-    serverLogger.color("magenta").write(`REDIRECT: ${url}`);
+    if (NodeEnv !== "production") {
+        serverLogger.color("magenta").write(`REDIRECT: ${url}`);
+    }
 
     const response = await fetch(`${req.protocol}://${req.headers.host}${url}`, {
         method: "GET",
